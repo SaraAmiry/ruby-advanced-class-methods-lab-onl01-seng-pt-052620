484a6deb6b0ed2c89863b37106055db01461d735
Sara Amiry <saraamiry24@gmail.com>
2020-06-24 20:06:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,6 +29,16 @@ class Song
   def self.find_by_name(name)
     self.all.find {|song| song.name == name}
   end 
+  
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end 
+  def self.new_from_filename(filename)
+    artist_name, name = filename.chomp('.mp3').split('-')
+    song = self.new_by_name(name)
+    song.artist_name = artist_name
+    song 
+  end
   def self.destroy_all
     @@all.clear 
   end
